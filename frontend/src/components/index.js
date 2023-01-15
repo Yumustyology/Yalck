103465dd6d2e881fe92267ff12cfc31d529e8b19
Yusuf <yumustyologt@example.com>
2023-01-15 09:23:20 +0100

    updates

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -8,5 +8,6 @@ export { default as CreateChannel } from './CreateChannel';
 export { default as EditChannel } from './EditChannel';
 export { default as TeamMessage } from './TeamMessage';
 export { default as Auth } from './Auth';
+export { default as Authentication } from './Authentication';
 export { default as UserList } from './UserList';
 export { default as ResultsDropdown } from './ResultsDropdown';
